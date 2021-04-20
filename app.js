0a9217bafc9b4b87b9e771dbca774d009f045bee
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-20 14:26:57 +0300

    added 1-st & 2-nd cases to navToggle

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
 // -------- close links --------
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", () => {
+	// 1-st case - but not very good (fixed height in styles)
+	// linksContainer.classList.toggle("show-links");
+
+	// 2-nd case - better (height is dynamic)
+	// Element.getBoundingClientRect() method returns the size of
+	// an element and its position relative to the viewport.
+	const containerHeight = linksContainer.getBoundingClientRect().height;
+	const linksHeight = links.getBoundingClientRect().height;
+
+	linksContainer.style.height = containerHeight === 0 ? `${linksHeight}px` : 0;
+});
+
+// -------- fixed navbar --------
